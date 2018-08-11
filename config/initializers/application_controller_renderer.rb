52014f1d14ded342cb6b0934645d6e2b80a9da51
jeffer480 <radiohead480@gmail.com>
2018-08-11 13:13:14 -0400

    asas

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ApplicationController::Base 
-end
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
